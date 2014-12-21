969e31ace6f6f74a48fcf5d92dc76ee5576ef50d
Machol <tianwen2976@yeah.net>
2014-12-21 13:04:18 +0800

    add branch

--- hello.py
+++ hello.py
@@ -1,2 +1,36 @@
 #!/usr/bin/env  python
 print "hello, world"
+
+class AddrBookEntry(object):
+    'address book entry class'
+
+    def __init__(self,nm,ph):
+        self.name = nm
+        self.phone = ph
+        print 'Created instance for:',self.name
+
+    def updatePhone(self,newph):
+        self.phone = newph
+        print 'Updated phone# for: ',self.phone
+
+class EmplAddrBookEntry(AddrBookEntry):
+    'Employ Address Book Entry class'
+
+    def __init__(self,nm,ph,id,em):
+        AddrBookEntry.__init__(self,nm,ph)
+        self.empid = id
+        self.email = em
+
+    def updateEmail(self,newem):
+        self.email = newem
+        print 'Updated e-mail address for:',self.name
+
+
+class MyClass:
+    def __init__(self):
+        print 'initialized'
+
+
+if __name__=='__main__':
+    mc = MyClass()
+
